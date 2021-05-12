82cc9ae1e7b3ea5ad6a5eff0b26e8a5d26820320
Kaitlyn Hiller <k.m.hiller@gmail.com>
2021-05-12 11:49:16 -0400

    add static function to find user

--- server/models/user.js
+++ server/models/user.js
@@ -36,7 +36,30 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    minlength: 8
   }
 });
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials (email, password) {
+  // trying to find the user by email
+  return this.findOne({ email }) // this — the User model
+    .then((user) => {
+      // not found - rejecting the promise
+      if (!user) {
+        return Promise.reject(new Error('Incorrect email or password'));
+      }
+
+      // found - comparing hashes
+      return bcrypt.compare(password, user.password)
+      .then((matched) => {
+        if(!matched) { // reject promise
+        return Promise.reject(new Error('Incorrect email or password'))
+        }
+
+        return user; 
+      })
+    })
+    
+};
 
 module.exports = mongoose.model('user', userSchema);
