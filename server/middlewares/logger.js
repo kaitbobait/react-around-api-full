18de72dfd98f552e9810410ef05d736ce55408a9
Kaitlyn Hiller <k.m.hiller@gmail.com>
2021-05-15 00:17:16 -0400

    install and add logger middleware

---
+++ server/middlewares/logger.js
@@ -0,0 +1,25 @@
+const winston = require('winston');
+const expressWinston = require('express-winston'); 
+
+// create a request logger
+const requestLogger = expressWinston.logger({
+  // where the logs are written
+  transports: [
+    new winston.transports.File({filename: 'request.log'}),
+  ],
+  //format of the logging
+  format: winston.format.json(),
+});
+
+// error logger
+const errorLogger = expressWinston.errorLogger({
+  transports: [
+    new winston.transports.File({ filename: 'error.log' }),
+  ],
+  format: winston.format.json(),
+}); 
+
+module.exports = {
+  requestLogger,
+  errorLogger,
+}; 
