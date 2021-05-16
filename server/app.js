6502663d9819f17560cc497694faf9e148c20f52
Kaitlyn Hiller <k.m.hiller@gmail.com>
2021-05-16 00:18:39 -0400

    remove more unique

--- server/app.js
+++ server/app.js
@@ -50,7 +50,7 @@ app.post(
   "/signup",
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().required().unique().email(),
+      email: Joi.string().required().email(),
       password: Joi.string().required().min(8),
     }),
   }),
