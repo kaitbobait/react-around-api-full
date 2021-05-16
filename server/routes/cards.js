82c26432b9db09526f6a334490fe14ee9c71e480
Kaitlyn Hiller <k.m.hiller@gmail.com>
2021-05-15 23:54:41 -0400

    add celebrate require

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -1,13 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../middlewares/auth');
+const auth = require("../middlewares/auth");
+const { celebrate, Joi } = require("celebrate");
 
 const {
-  getCards, createCard, deleteCard, addLike, deleteLike,
-} = require('../controllers/cardsControllers');
+  getCards,
+  createCard,
+  deleteCard,
+  addLike,
+  deleteLike,
+} = require("../controllers/cardsControllers");
 
-router.get('/cards', auth, getCards);
-router.post('/cards', auth, 
+router.get("/cards", auth, getCards);
+router.post(
+  "/cards",
+  auth,
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().required().min(2).max(30),
@@ -18,12 +25,12 @@ router.post('/cards', auth,
 );
 
 //QUESTION do I need to add params or headers here?
-router.delete('/cards/:cardId', auth, deleteCard);
+router.delete("/cards/:cardId", auth, deleteCard);
 
 // like a card
-router.put('/cards/:cardId/likes', auth, addLike);
+router.put("/cards/:cardId/likes", auth, addLike);
 
 // delete user like from card
-router.delete('/cards/:cardId/likes', auth, deleteLike);
+router.delete("/cards/:cardId/likes", auth, deleteLike);
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/auth");
+const { celebrate, Joi } = require("celebrate");
 
 const {
   getUsers,
