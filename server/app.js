3681647f9f5c908233c7ffa048b0eb8b5f0ed054
Kaitlyn Hiller <k.m.hiller@gmail.com>
2021-05-16 00:17:34 -0400

    remove .unique

--- server/app.js
+++ server/app.js
@@ -40,7 +40,7 @@ app.post(
   "/signin",
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().required().unique().email(),
+      email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
   }),
